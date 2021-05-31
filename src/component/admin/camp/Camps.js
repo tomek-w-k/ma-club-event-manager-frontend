f34abcd76c8e76c3c760405d82c45c5bf315e88d
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2021-05-31 23:41:05 +0200

    Implement image handling for Camp events
    Now it's possible to set a picture either while creating a new camp
    or editing it. When a camp is removed, its picture is removed too.

--- src/component/admin/camp/Camps.js
+++ src/component/admin/camp/Camps.js
@@ -63,9 +63,21 @@ class CampsComponent extends Component
                 }
             })
             .then(result => {
-                this.setState({ selectedRowsIds: [] });
-                this.crudTableRef.current.unselectAllRows();
-                this.crudTableRef.current.fillTable();
+                let formData = new FormData();
+                formData.append("dir", "/images/camps/" + this.state.selectedRowsIds[0]);
+
+                fetch(Urls.EXPRESS_JS_URL + "/clear_dir", {
+                    method: "DELETE",
+                    headers: {
+                        "Authorization": "Bearer " + currentUser.accessToken
+                    },
+                    body: formData
+                })
+                .then(() => { 
+                    this.setState({ selectedRowsIds: [] });
+                    this.crudTableRef.current.unselectAllRows();
+                    this.crudTableRef.current.fillTable();    
+                });
             },
             error => {
                 console.log("Item not deleted");
